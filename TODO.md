87dc76db739474c30c34a37a362e2729c942a038
Saige Cross <saigecrossdev@gmail.com>
2024-07-17 11:52:08 -0400

    fix new user callback

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# Todos
+
+- [ ] Create 'New User' flow
+- [ ]
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -72,7 +72,8 @@ export const authOptions: NextAuthOptions = {
     // signOut: "/auth/signout",
     error: "/auth/error", // Error code passed in query string as ?error=
     verifyRequest: "/auth/verify-request", // (used for check email message)
-    newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+    // TODO: Create a new user flow
+    // newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
   },
 };
 
