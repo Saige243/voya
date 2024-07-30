17e59ad2b3092155a2055be2f37d6e23969f6611
Saige Cross <saigecrossdev@gmail.com>
2024-07-30 17:14:42 -0500

    correctly fetching trip

--- src/app/trips/[id]/page.tsx
+++ src/app/trips/[id]/page.tsx
@@ -1,5 +1,27 @@
-export default function Page({ params }: { params: { slug: string } }) {
+import { api } from "~/trpc/server";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  async function getTrip() {
+    "use server";
+    const { id } = params;
+    const tripId = parseInt(id);
+
+    console.log("IDDDD", tripId);
+
+    const trip = await api.trip.getById({ id: tripId });
+    try {
+      console.log("trip", trip);
+      return trip;
+    } catch (error) {
+      throw new Error("Trip not found");
+    }
+  }
+
+  const trip = await getTrip();
+
   return (
-    <main className="flex min-h-screen flex-col">My Trip: {params.slug}</main>
+    <main className="flex min-h-screen justify-center">
+      <h1>My Trip: {trip?.title}</h1>
+    </main>
   );
 }
