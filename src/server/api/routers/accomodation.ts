b0701e23f2264d51f489c2d74ad1fc0a73e05301
Saige Cross <saigecrossdev@gmail.com>
2024-09-01 15:08:26 -0500

    add accomodation router

---
+++ src/server/api/routers/accomodation.ts
@@ -0,0 +1,77 @@
+import { type Accommodation } from "@prisma/client";
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const accommodationRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string().min(1),
+        location: z.string().min(1),
+        checkIn: z.date(),
+        checkOut: z.date(),
+        notes: z.string(),
+        phoneNumber: z.string(),
+        website: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }): Promise<Accommodation> => {
+      const accommodation = ctx.db.accommodation.create({
+        data: {
+          name: input.name,
+          location: input.location,
+          checkIn: input.checkIn,
+          checkOut: input.checkOut,
+          notes: input.notes,
+          phoneNumber: input.phoneNumber,
+          website: input.website,
+          userId: ctx.session.user.id,
+        },
+      });
+      return accommodation;
+    }),
+
+  getAll: protectedProcedure.query(({ ctx }) => {
+    const allAccommodations = ctx.db.accommodation.findMany({
+      where: { userId: ctx.session.user.id },
+    });
+    return allAccommodations;
+  }),
+
+  getById: protectedProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ ctx, input }) => {
+      return ctx.db.accommodation.findUnique({
+        where: { id: input.id, userId: ctx.session.user.id },
+      });
+    }),
+
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        name: z.string().min(1).optional(),
+        location: z.string().min(1).optional(),
+        checkIn: z.date().optional(),
+        checkOut: z.date().optional(),
+        notes: z.string().optional(),
+        phoneNumber: z.string().optional(),
+        website: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id, ...data } = input;
+      return ctx.db.accommodation.update({
+        where: { id },
+        data,
+      });
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({ id: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.accommodation.delete({
+        where: { id: input.id },
+      });
+    }),
+});
