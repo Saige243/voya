eee929b3f76ee54306e2f3b7cde70098624b19df
Saige Cross <saigecrossdev@gmail.com>
2024-09-30 12:00:14 -0500

    form cleanup

--- src/app/_components/Card.tsx
+++ src/app/_components/Card.tsx
@@ -14,8 +14,8 @@ export function Card({
       className={`card h-fit bg-base-100 shadow-xl dark:text-white ${className}`}
     >
       <div className="card-body">
-        <h2 className="card-title dark:text-white">{title}</h2>
-        <p className="pb-4">{description}</p>
+        <h2 className="card-title text-2xl dark:text-white">{title}</h2>
+        <p className="pb-4 text-lg font-semibold">{description}</p>
         {children}
       </div>
     </div>
--- src/app/trips/_components/TripCard.tsx
+++ src/app/trips/_components/TripCard.tsx
@@ -6,13 +6,49 @@ function TripCard(trip: Trip) {
   return (
     <div className="text-black">
       <a href={`/trips/${trip.id}`}>
-        <Card title={trip.title} description={trip.description}>
-          <div>
-            <h2>{trip.title}</h2>
-            <p>{trip.destination}</p>
-            <p>{trip.description}</p>
-            <p>{trip.startDate.toString()}</p>
-            <p>{trip.endDate.toString()}</p>
+        <Card
+          title={trip.title}
+          description={trip.description}
+          className="w-[500px]"
+        >
+          <div className="grid grid-cols-2 gap-6">
+            <div>
+              <label className="block text-sm text-gray-500" htmlFor="title">
+                Title:
+              </label>
+              <h1 className="mb-2 text-xl font-bold">{trip.title}</h1>
+
+              <label
+                className="block text-sm text-gray-500"
+                htmlFor="destination"
+              >
+                Destination:
+              </label>
+              <h2 className="mb-2 font-semibold">{trip.destination}</h2>
+
+              <label
+                className="block text-sm text-gray-500"
+                htmlFor="description"
+              >
+                Description:
+              </label>
+              <p className="text-gray-700">{trip.description}</p>
+            </div>
+
+            <div>
+              <label
+                className="block text-sm text-gray-500"
+                htmlFor="start-date"
+              >
+                Start Date:
+              </label>
+              <p className="mb-2">{trip.startDate.toDateString()}</p>
+
+              <label className="block text-sm text-gray-500" htmlFor="end-date">
+                End Date:
+              </label>
+              <p>{trip.endDate.toDateString()}</p>
+            </div>
           </div>
         </Card>
       </a>
