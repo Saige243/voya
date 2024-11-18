a42735df8acceeac8ff89270b8192c42aca382a1
Saige Cross <saigecrossdev@gmail.com>
2024-11-18 13:01:26 -0600

    fixing styles for card

--- src/app/_components/ui/Card.tsx
+++ src/app/_components/ui/Card.tsx
@@ -9,15 +9,19 @@ export function Card({
   children: React.ReactNode;
   className?: string;
 }) {
+  const cardHeader = (
+    <>
+      <h2 className="card-title text-2xl dark:text-white">{title}</h2>
+      <p className="pb-4 text-lg font-semibold">{description}</p>
+    </>
+  );
+
   return (
     <div
       className={`card h-fit bg-base-100 shadow-xl dark:text-white ${className}`}
     >
-      <div className="card-body">
-        <h2 className="card-title text-2xl dark:text-white">{title}</h2>
-        <p className="pb-4 text-lg font-semibold">{description}</p>
-        {children}
-      </div>
+      {title && description && <div className="card-header">{cardHeader}</div>}
+      <div className="card-body">{children}</div>
     </div>
   );
 }
--- src/app/trips/_components/TripCard.tsx
+++ src/app/trips/_components/TripCard.tsx
@@ -1,30 +1,30 @@
 import React from "react";
 import { type Trip } from "@prisma/client";
 import { Card } from "~/app/_components/ui/Card";
+import { format } from "date-fns";
 
 function TripCard(trip: Trip) {
   return (
     <div className="text-black">
       <a href={`/trips/${trip.id}`}>
         <Card
-          title={trip.title}
-          description={trip.description}
+          // title={trip.title}
+          // description={trip.description}
           className="w-[500px]"
         >
           <div className="grid grid-cols-2 gap-6">
             <div>
-              <label className="block text-sm text-gray-500" htmlFor="title">
-                Title:
-              </label>
-              <h1 className="mb-2 text-xl font-bold">{trip.title}</h1>
-
               <label
                 className="block text-sm text-gray-500"
                 htmlFor="destination"
               >
                 Destination:
               </label>
-              <h2 className="mb-2 font-semibold">{trip.destination}</h2>
+              <h2 className="mb-2 text-xl font-semibold">{trip.destination}</h2>
+              <label className="block text-sm text-gray-500" htmlFor="title">
+                Title:
+              </label>
+              <h2 className="mb-2 font-bold">{trip.title}</h2>
 
               <label
                 className="block text-sm text-gray-500"
@@ -32,7 +32,7 @@ function TripCard(trip: Trip) {
               >
                 Description:
               </label>
-              <p className="text-gray-700">{trip.description}</p>
+              <h2 className="mb-2 font-semibold">{trip.description}</h2>
             </div>
 
             <div>
@@ -42,12 +42,14 @@ function TripCard(trip: Trip) {
               >
                 Start Date:
               </label>
-              <p className="mb-2">{trip.startDate.toDateString()}</p>
+              <p className="mb-2">
+                {format(new Date(trip.startDate), "EEEE, MMMM d, yyyy")}
+              </p>
 
               <label className="block text-sm text-gray-500" htmlFor="end-date">
                 End Date:
               </label>
-              <p>{trip.endDate.toDateString()}</p>
+              <p>{format(new Date(trip.endDate), "EEEE, MMMM d, yyyy")}</p>
             </div>
           </div>
         </Card>
