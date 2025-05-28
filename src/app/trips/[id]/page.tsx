0fedbb3b9c8a976f138c71aa6f960b58414cb675
Saige Cross <saigecrossdev@gmail.com>
2025-05-28 10:30:37 -0500

    cleanup trip details page

--- src/app/trips/[id]/page.tsx
+++ src/app/trips/[id]/page.tsx
@@ -5,7 +5,7 @@ import { getServerAuthSession } from "~/server/auth";
 import { Icon } from "~/app/_components/common/Icon";
 import ItineraryBlock from "../../_components/itineraries/ItineraryBlock";
 import { type Accommodation } from "@prisma/client";
-import AccommodationList from "../../_components/accommodations/AccommodationList";
+import { DeleteAccommodationButton } from "~/app/_components/accommodations/DeleteAccommodationButton";
 import { format } from "date-fns";
 import { Label } from "~/components/ui/label";
 import { Typography } from "~/app/_components/common/Typography";
@@ -13,41 +13,39 @@ import BackButton from "../../_components/trips/BackButton";
 import { DeleteTripButton } from "~/app/_components/trips/DeleteTripButton";
 import { Card, CardContent } from "~/components/ui/card";
 
-export default async function Page({ params }: { params: { id: string } }) {
+type AccommodationListProps = {
+  accommodations: Accommodation[];
+  tripId: number;
+};
+
+async function getTrip(id: string) {
+  const tripId = parseInt(id);
+  const trip = await api.trip.getById({ id: tripId });
+  if (!trip) throw new Error("Trip not found");
+  return trip;
+}
+
+async function getAccommodations(id: string) {
+  const tripId = parseInt(id);
+  try {
+    return await api.accommodation.getAll({ tripId });
+  } catch (error) {
+    console.error("Error fetching accommodations:", error);
+    throw new Error("No accommodations found");
+  }
+}
+
+export default async function TripDetailsPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const trip = await getTrip(params.id);
+  const accommodations = await getAccommodations(trip.id.toString());
   const session = await getServerAuthSession();
   if (!session) {
     redirect("/");
   }
-
-  async function getTrip() {
-    "use server";
-    const { id } = params;
-    const tripId = parseInt(id);
-
-    const trip = await api.trip.getById({ id: tripId });
-    if (!trip) {
-      throw new Error("Trip not found");
-    }
-    return trip;
-  }
-
-  const trip = await getTrip();
-
-  async function getAccommodations(): Promise<Accommodation[]> {
-    "use server";
-    const { id } = params;
-    const tripId = parseInt(id);
-
-    try {
-      const accommodations = await api.accommodation.getAll({ tripId });
-      return accommodations;
-    } catch (error) {
-      console.error("Error fetching accommodations:", error);
-      throw new Error("No accommodations found");
-    }
-  }
-
-  const accommodations = await getAccommodations();
 
   const editButtons = (
     <div className="flex items-center">
@@ -96,6 +94,98 @@ export default async function Page({ params }: { params: { id: string } }) {
       </CardContent>
     </Card>
   );
+
+  function AccommodationList({
+    accommodations,
+    tripId,
+  }: AccommodationListProps) {
+    const accommodationInfo = (acc: Accommodation) => (
+      <Card>
+        <CardContent>
+          <div className="flex items-center justify-between">
+            <Typography variant="heading1">{acc.name}</Typography>
+          </div>
+          <Typography>{acc.location}</Typography>
+          <div className="mb-4 mt-4 grid grid-cols-2 gap-4">
+            <div>
+              <Label htmlFor="check-in-date">Check-In:</Label>
+              <Typography>
+                {format(new Date(acc.checkIn), "MMM dd, yyyy")}
+              </Typography>
+            </div>
+            <div>
+              <Label htmlFor="check-out-date">Check-Out:</Label>
+              <Typography>
+                {format(new Date(acc.checkOut), "MMM dd, yyyy")}
+              </Typography>
+            </div>
+          </div>
+          {acc.notes && (
+            <div className="mb-4">
+              <Label htmlFor="notes">Notes:</Label>
+              <Typography>{acc.notes}</Typography>
+            </div>
+          )}
+          {acc.phoneNumber && (
+            <div className="mb-4">
+              <Label htmlFor="phone-number">Phone:</Label>
+              <Typography>{acc.phoneNumber}</Typography>
+            </div>
+          )}
+          {acc.website && (
+            <div className="mb-4 flex flex-col">
+              <Label htmlFor="website">Website: </Label>
+              <a
+                href={acc.website}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                {acc.name + " Website"}
+              </a>
+              <div className="mt-2 flex justify-end">
+                {editTripButton}
+                <DeleteAccommodationButton accId={acc.id} />
+              </div>
+            </div>
+          )}
+        </CardContent>
+      </Card>
+    );
+
+    const editTripButton = (
+      <a href={`/trips/${tripId}/edit`}>
+        <Button variant="ghost">
+          <Icon
+            name="Pencil"
+            className="text-black dark:text-white"
+            size="20"
+          />
+        </Button>
+      </a>
+    );
+
+    return (
+      <div className="w-full">
+        {accommodations.length > 0 ? (
+          <div>
+            {accommodations.map((acc) => (
+              <div key={acc.id}>{accommodationInfo(acc)}</div>
+            ))}
+          </div>
+        ) : (
+          <div className="text-center">
+            <p className="text-gray-600">No accommodations!</p>
+            <Button variant="default">
+              <a href={`/trips/${tripId}/add-accommodation`}>
+                Add Accommodation
+              </a>
+            </Button>
+          </div>
+        )}
+      </div>
+    );
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center">
