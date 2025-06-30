2982ce699ae3cff2de296cdf6f33c8030ddb7e26
Saige Cross <saigecrossdev@gmail.com>
2025-06-30 08:20:58 -0500

    fix time issue on server

--- src/components/itineraries/ItineraryForm.tsx
+++ src/components/itineraries/ItineraryForm.tsx
@@ -9,15 +9,30 @@ const AddItineraryItemForm = ({ trip }: { trip: Trip }) => {
   async function addItinerary(formData: FormData) {
     "use server";
 
-    const rawDate = formData.get("datetime") as string;
-    const date = new Date(rawDate).toISOString();
+    const rawDateTime = formData.get("datetime") as string;
     // const tripStartDate = trip.startDate;
     // const tripEndDate = trip.endDate;
+
+    if (!rawDateTime || isNaN(new Date(rawDateTime).getTime())) {
+      console.error("Invalid date and time provided");
+      return;
+    }
+
+    const dateTime = new Date(rawDateTime);
+    const dateOnly = new Date(
+      dateTime.getFullYear(),
+      dateTime.getMonth(),
+      dateTime.getDate(),
+    );
+    const date = dateOnly.toISOString();
+
+    const time = rawDateTime;
 
     const itinerarItemData = {
       tripId: trip.id,
       title: formData.get("title") as string,
       date: date,
+      time: time,
       location: formData.get("location") as string,
       notes: formData.get("notes") as string,
     };
