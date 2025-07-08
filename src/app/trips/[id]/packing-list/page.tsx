5646d662467a29faf539c5fb78b905cb533d1069
Saige Cross <saigecrossdev@gmail.com>
2025-07-08 11:16:03 -0500

    fix client rendering in packing list

--- src/app/trips/[id]/packing-list/page.tsx
+++ src/app/trips/[id]/packing-list/page.tsx
@@ -1,6 +1,14 @@
+"use client";
+
 import React from "react";
+import getPackingList from "../../actions/getPackingList";
+import { useTrip } from "~/app/trips/contexts/TripContext";
 
 function PackingListPage() {
+  const { trip } = useTrip();
+  const tripId = trip?.id?.toString() ?? "";
+  const packingList = getPackingList(tripId);
+
   const emptyList = (
     <div>
       <p className="text-gray-500">Your packing list is empty.</p>
