b8d44d3825561159b964ca5184fa802ba836e972
Saige Cross <saigecrossdev@gmail.com>
2025-07-14 18:31:00 -0500

    add select

--- src/app/trips/[id]/packing-list/add-item/page.tsx
+++ src/app/trips/[id]/packing-list/add-item/page.tsx
@@ -7,6 +7,7 @@ import {
 } from "@radix-ui/react-dropdown-menu";
 import React from "react";
 import { useTrip } from "~/app/trips/contexts/TripContext";
+import { Button } from "~/components/ui/button";
 import { DropdownMenu } from "~/components/ui/dropdown-menu";
 import { Input } from "~/components/ui/input";
 
@@ -18,13 +19,20 @@ function PackingListPage() {
   return (
     <main className="flex min-h-full flex-col items-center justify-center">
       <h1 className="text-2xl font-bold">Packing List</h1>
-      <Input placeholder="Add a new packing list item" />
-      <DropdownMenu>
-        <DropdownMenuTrigger>Add Item</DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem>Add to Packing List</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <div className="mt-4 flex w-full items-center space-x-2">
+        <Input placeholder="Add a new packing list item" />
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            className="w-full focus:outline-none focus:ring-0"
+            asChild
+          >
+            <Button>Select a category</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-100 bg-white p-4">
+            <DropdownMenuItem className="text-black">Sign out</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </main>
   );
 }
